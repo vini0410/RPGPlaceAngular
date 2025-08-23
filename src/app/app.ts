13cd4ee3b6adc4bcc835e311de92de28b0523b1b
vini0410 <vini0420@gmail.com>
2025-08-23 14:09:49 -0300

    init project dark theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,40 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnDestroy {
   protected readonly title = signal('RPGPlaceAngular');
+  themeText = 'Modo Claro';
+  private observer!: MutationObserver;
+
+  ngOnInit() {
+    this.updateThemeText();
+
+    this.observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        if (mutation.attributeName === 'class') {
+          this.updateThemeText();
+        }
+      });
+    });
+
+    this.observer.observe(document.documentElement, {
+      attributes: true
+    });
+  }
+
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
+
+  toggleTheme() {
+    document.documentElement.classList.toggle('dark');
+  }
+
+  private updateThemeText() {
+    if (document.documentElement.classList.contains('dark')) {
+      this.themeText = 'Modo Escuro';
+    } else {
+      this.themeText = 'Modo Claro';
+    }
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '.dark'],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
